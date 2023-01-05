7e9730c35318fa6aaa61ee4ee24a61fd0a5627bd
Ally <alisa.luna27@gmail.com>
2023-01-05 17:52:16 +0800

    edited socket io url

--- src/pages/OnlineSession.js
+++ src/pages/OnlineSession.js
@@ -198,7 +198,7 @@ const fetchFirst = async (e) => {
 
 //-------------SERVER CONNECTION---------------
     //new 12-7
-    const socket = io.connect('https://charming-paprenjak-891a84.netlify.app');
+    const socket = io.connect('https://mernsample-tool.adaptable.app');
     
     // kay useeffect dapat ung paghcnage ng leaderbpards
     useEffect(() => {
